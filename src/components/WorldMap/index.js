db5d76545265ac05a29629ad8f9d1be63a326d84
Nick Rutten <hello@nickrttn.me>
2017-01-18 20:51:32 +0100

    moved the users location indicator to its own component

--- src/components/WorldMap/index.js
+++ src/components/WorldMap/index.js
@@ -1,8 +1,10 @@
-/*eslint react/prefer-stateless-function:0 */
+/* eslint react/prefer-stateless-function:0 */
 import React, { Component } from 'react';
 import superagent from 'superagent';
-import { mesh } from 'topojson';
+import { feature, mesh } from 'topojson';
 import { geoPath, geoMercator } from 'd3-geo';
+
+import UserLocation from '../UserLocation';
 
 import styles from './styles.scss';
 
@@ -11,6 +13,18 @@ class WorldMap extends Component {
     super(props);
     // initialize an empty state so we can keep render clean(er)
     this.state = {};
+    this.countriesWarCrimes = [
+      'Afghanistan',
+      'Burundi',
+      'Colombia',
+      'Gabon',
+      'Guinea',
+      'Iraq',
+      'United Kingdom',
+      'Nigeria',
+      'Palestine',
+      'Ukraine',
+    ];
   }
 
   fetchGeodata() {
@@ -19,46 +33,57 @@ class WorldMap extends Component {
       .then(geoData => this.setState({ geoData }));
   }
 
-  requestUserLocation() {
-    const projection = geoMercator();
+  filterGeoData() {
+    const { geoData } = this.state;
+    const path = geoPath(geoMercator());
 
-    if (navigator && 'geolocation' in navigator) {
-      /* geolocation is available */
-      navigator.geolocation.getCurrentPosition((position) => {
-        const lonLat = [position.coords.longitude, position.coords.latitude];
-        // great, we have geolocation, save to state
-        this.setState({ userLocation: projection(lonLat) });
-        // user did not give permissions, abort
-      }, () => this.setState({ userLocation: false }));
-    } else {
-      // no geolocation, abort
-      this.setState({ userLocation: false });
-    }
+    // Convert the geodata to GeoJSON
+    const featureCollection = feature(geoData, geoData.objects.geoCountries);
+
+    // Filter the GeoJSON by countries war crimes have been committed in
+    const filteredFeatures = featureCollection.features.filter(country => {
+      const countryName = country.properties.ADMIN;
+      return this.countriesWarCrimes.includes(countryName);
+    });
+
+    const pathCenters = filteredFeatures.map((country) => {
+      return {
+        name: country.properties.ADMIN,
+        center: path.centroid(country),
+      };
+    });
+
+    return pathCenters;
   }
 
   componentDidMount() {
     // fetch the data needed to render countries
     this.fetchGeodata();
-    // and request the users' location
-    this.requestUserLocation();
   }
 
   render() {
-    const { userLocation, geoData } = this.state;
+    const { geoData } = this.state;
     const path = geoPath(geoMercator());
 
     return (
       <section className={styles.WorldMapContainer}>
+        <div className={styles.WorldMapContent}></div>
+        <div className={styles.WorldMap}></div>
         { geoData &&
           <svg
-            className={styles.WorldMap}
+            className={styles.WorldMapSVG}
             viewBox="0 0 961 620" preserveAspectRatio="xMidYMin slice"
           >
             <g className={styles.WorldMapCountries} transform="translate(0, 192)">
               <path d={path(mesh(geoData, geoData.objects.geoCountries))} />
-              { userLocation &&
-                <circle className={styles.WorldMapUserPosition} r="2" stroke="none" cx={`${userLocation[0]}`} cy={`${userLocation[1]}`} />
-              }
+              <UserLocation />
+              { this.filterGeoData().map(country => <circle
+                key={country.name}
+                className={styles.WorldMapCountryHighlight}
+                r="3" fill="blue" stroke="none"
+                cx={`${country.center[0]}`}
+                cy={`${country.center[1]}`} />
+              )}
             </g>
           </svg>
         }
