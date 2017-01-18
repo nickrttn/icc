4f564b687233b2dfa9aa7547ca85a65245dedb16
Nick Rutten <hello@nickrttn.me>
2017-01-18 18:39:39 +0100

    updated tests for Introduction

--- src/components/Introduction/index.test.js
+++ src/components/Introduction/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Introduction from './';
 
-it('has an article', () => {
+it('has a section', () => {
   const wrapper = shallow(<Introduction />);
-  expect(wrapper.is('article')).toBeTruthy();
+  expect(wrapper.is('section')).toBeTruthy();
 });
