424ab13d49dce5a80090c9ee65c02aa455fb5364
Nick Rutten <hello@nickrttn.me>
2017-01-18 16:15:17 +0100

    preliminary implementation of WorldMap with geolocation

--- src/components/WorldMap/index.js
+++ src/components/WorldMap/index.js
@@ -7,27 +7,61 @@ import { geoPath, geoMercator } from 'd3-geo';
 import styles from './styles.scss';
 
 class WorldMap extends Component {
+  constructor(props) {
+    super(props);
+    // initialize an empty state so we can keep render clean(er)
+    this.state = {};
+  }
+
   fetchGeodata() {
-    superagent('get', 'https://d3js.org/world-110m.v1.json')
+    superagent('get', 'data/geo-countries.json')
       .then(result => JSON.parse(result.text))
-      .then(data => this.setState({ geoData: mesh(data, data.objects.countries) }));
+      .then(geoData => this.setState({ geoData }));
+  }
+
+  requestUserLocation() {
+    const projection = geoMercator();
+
+    if (navigator && 'geolocation' in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lonLat = [position.coords.longitude, position.coords.latitude];
+        // great, we have geolocation, save to state
+        this.setState({ userLocation: projection(lonLat) });
+        // user did not give permissions, abort
+      }, () => this.setState({ userLocation: false }));
+    } else {
+      // no geolocation, abort
+      this.setState({ userLocation: false });
+    }
   }
 
   componentDidMount() {
+    // fetch the data needed to render countries
     this.fetchGeodata();
+    // and request the users' location
+    this.requestUserLocation();
   }
 
   render() {
-
+    const { userLocation, geoData } = this.state;
     const path = geoPath(geoMercator());
 
     return (
       <section className={styles.WorldMapContainer}>
-      { this.state && this.state.geoData &&
-        <svg className={styles.WorldMap}>
-          <path d={ path(this.state.geoData) }></path>
-        </svg>
-      }
+        { geoData &&
+          <svg
+            className={styles.WorldMap}
+            viewBox="0 0 961 620" preserveAspectRatio="xMidYMin slice"
+          >
+            <g className={styles.WorldMapCountries} transform="translate(0, 192)">
+              <path d={path(mesh(geoData, geoData.objects.geoCountries))} />
+              { userLocation &&
+                <circle className={styles.WorldMapUserPosition} r="2" stroke="none" cx={`${userLocation[0]}`} cy={`${userLocation[1]}`} />
+              }
+            </g>
+          </svg>
+        }
       </section>
     );
   }
