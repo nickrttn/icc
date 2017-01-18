fc15f149790826823697d43abf5d3afdcd676dff
Nick Rutten <hello@nickrttn.me>
2017-01-18 10:37:18 +0100

    remove the build folder from version control

--- src/components/WorldMap/index.test.js
+++ src/components/WorldMap/index.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './';
+import WorldMap from './';
 
 it('Test example', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.is('div')).toBeTruthy();
+  const wrapper = shallow(<WorldMap />);
+  expect(wrapper.is('section')).toBeTruthy();
 });
