b571f547b40559863bce46e562ef96b5e941c501
Nick Rutten <hello@nickrttn.me>
2017-01-18 10:50:18 +0100

    updated the WorldMap test

--- src/components/WorldMap/index.test.js
+++ src/components/WorldMap/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './';
 
-it('Test example', () => {
+it('has a section', () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.is('div')).toBeTruthy();
+  expect(wrapper.is('section')).toBeTruthy();
 });
