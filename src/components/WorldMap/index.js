97e0add50be4f0fe70d0cf860f1281f6978f88f2
Nick Rutten <hello@nickrttn.me>
2017-01-22 16:38:33 +0100

    put the UserLocation back into WorldMap and calculate closest war crime to the user

--- src/components/WorldMap/index.js
+++ src/components/WorldMap/index.js
@@ -1,10 +1,8 @@
 /* eslint react/prefer-stateless-function:0 */
 import React, { Component } from 'react';
 import superagent from 'superagent';
-import { feature, mesh } from 'topojson';
+import { feature } from 'topojson';
 import { geoPath, geoMercator } from 'd3-geo';
-
-import UserLocation from '../UserLocation';
 
 import styles from './styles.scss';
 
@@ -12,7 +10,10 @@ class WorldMap extends Component {
   constructor(props) {
     super(props);
     // initialize an empty state so we can keep render clean(er)
-    this.state = {};
+    this.state = {
+      userLocation: false,
+    };
+
     this.countriesWarCrimes = [
       'Afghanistan',
       'Burundi',
@@ -28,32 +29,80 @@ class WorldMap extends Component {
   }
 
   fetchGeodata() {
-    superagent('get', 'data/geo-countries.json')
+    return superagent('get', 'data/geo-countries.json')
       .then(result => JSON.parse(result.text))
+      .then(geoData => this.getFeatureCollection(geoData))
       .then(geoData => this.setState({ geoData }));
   }
 
-  filterGeoData() {
-    const { geoData } = this.state;
-    const path = geoPath(geoMercator());
+  getProjection() {
+    const projection = geoMercator()
+      .fitExtent([[20, 20], [961, 620]], this.state.geoData)
+      .rotate([-11, 0, 0]);
 
-    // Convert the geodata to GeoJSON
+    return {
+      scale: projection.scale(),
+      translate: projection.translate(),
+      rotation: projection.rotate(),
+    };
+  }
+
+  getPath() {
+    const projection = geoMercator()
+      .fitExtent([[20, 20], [961, 620]], this.state.geoData)
+      .rotate([-11, 0, 0]);
+    const path = geoPath(projection);
+    return path;
+  }
+
+  getFeatureCollection(geoData) {
     const featureCollection = feature(geoData, geoData.objects.geoCountries);
+    featureCollection.features = featureCollection.features.filter(country => country.geometry);
+    return featureCollection;
+  }
+
+  renderFeatureCollection() {
+    const path = this.getPath();
+    const features = this.state.geoData.features.map(country => (
+      <path key={country.properties.ADMIN} d={path(country)} />
+    ));
+
+    return features;
+  }
+
+  filterGeoData() {
+    const path = this.getPath();
 
     // Filter the GeoJSON by countries war crimes have been committed in
-    const filteredFeatures = featureCollection.features.filter(country => {
+    const filteredFeatures = this.state.geoData.features.filter((country) => {
       const countryName = country.properties.ADMIN;
       return this.countriesWarCrimes.includes(countryName);
     });
 
-    const pathCenters = filteredFeatures.map((country) => {
-      return {
-        name: country.properties.ADMIN,
-        center: path.centroid(country),
-      };
-    });
+    const pathCenters = filteredFeatures.map(country => ({
+      name: country.properties.ADMIN,
+      center: path.centroid(country),
+    }));
 
     return pathCenters;
+  }
+
+  requestUserLocation() {
+    const { scale, rotation, translate } = this.getProjection();
+
+    const projection = geoMercator();
+    projection.scale(scale);
+    projection.rotate(rotation);
+    projection.translate(translate);
+
+    if (navigator && 'geolocation' in navigator) {
+      // geolocation is available
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lonLat = [position.coords.longitude, position.coords.latitude];
+        // great, we have geolocation, save to state
+        this.setState({ userLocation: projection(lonLat) });
+      });
+    }
   }
 
   componentDidMount() {
@@ -61,26 +110,62 @@ class WorldMap extends Component {
     this.fetchGeodata();
   }
 
+  componentDidUpdate() {
+    this.requestUserLocation();
+
+    const user = this.User;
+    const warCrimes = Array.from(this.WarCrimes.querySelectorAll('circle'));
+
+    if (user && warCrimes) {
+      const userX = user.cx.baseVal.value;
+      const userY = user.cy.baseVal.value;
+
+      warCrimes.forEach((crime) => {
+        const crimeX = crime.cx.baseVal.value;
+        const crimeY = crime.cy.baseVal.value;
+        crime.distanceToUser = Math.sqrt(
+          ((userX - crimeX) * (userX - crimeX)) +
+          ((userY - crimeY) * (userY - crimeY))
+        );
+      });
+
+      warCrimes.sort((crimeA, crimeB) => {
+        return crimeA.distanceToUser - crimeB.distanceToUser;
+      });
+
+      warCrimes[0].style = 'fill: blue;';
+    }
+  }
+
   render() {
     const { geoData } = this.state;
-    const path = geoPath(geoMercator());
 
     return (
       <section className={styles.WorldMapContainer}>
-        <div className={styles.WorldMapContent}></div>
-        <div className={styles.WorldMap}></div>
         { geoData &&
           <svg
             className={styles.WorldMapSVG}
             viewBox="0 0 961 620" preserveAspectRatio="xMidYMin slice"
+            ref={(svg) => { this.SVG = svg; }}
           >
-            <g className={styles.WorldMapCountries} transform="translate(0, 192)">
-              <path d={path(mesh(geoData, geoData.objects.geoCountries))} />
-              <UserLocation />
+            <g className={styles.WorldMapCountries}>
+              { this.renderFeatureCollection() }
+            </g>
+
+            { this.state.userLocation &&
+              <g className={styles.WorldMapUser}>
+                <circle
+                  ref={(ref) => { this.User = ref; }} r="3"
+                  cx={this.state.userLocation[0]}
+                  cy={this.state.userLocation[1]}
+                />
+              </g>
+            }
+
+            <g ref={(ref) => { this.WarCrimes = ref; }} className={styles.WorldMapWarCrimes}>
               { this.filterGeoData().map(country =>
                 <circle
                   key={country.name}
-                  className={styles.WorldMapCountryHighlight}
                   r="3" fill="blue" stroke="none"
                   cx={`${country.center[0]}`}
                   cy={`${country.center[1]}`}
