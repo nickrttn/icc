0ba7c1a36fd62e8e744d4b3525ae19bab1fe7530
Nick Rutten <hello@nickrttn.me>
2017-01-25 21:12:14 +0100

    Process switching

--- src/components/Process/index.js
+++ src/components/Process/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'classnames/bind';
 import styles from './styles.scss';
 
 import ProcessProgress from '../ProcessProgress';
@@ -13,7 +14,23 @@ class Process extends Component {
     }
   }
 
+  setProcessType(type) {
+    this.setState({ process: type });
+  }
+
   render () {
+    let cx = classNames.bind(styles);
+
+    let countryButtonClass = cx({
+      ProcessChoice: true,
+      ProcessChoiceActive: this.state.process === 'country',
+    });
+
+    let individualButtonClass = cx({
+      ProcessChoice: true,
+      ProcessChoiceActive: this.state.process === 'individual',
+    });
+
     return (
       <section className={styles.Process}>
         <header className={styles.ProcessHeader}>
@@ -28,20 +45,42 @@ class Process extends Component {
           <div className={styles.ProcessFirstStep}>
             <h3>Choose your path</h3>
 
+            <div className={styles.ProcessChoices}>
+              <div className={styles.ProcessChoiceContainer}>
+                <button className={countryButtonClass} onClick={() => this.setProcessType('country')}>
+                  <div className={styles.ProcessChoiceBox}></div>
+                  <div className={styles.ProcessChoiceText}>
+                    <p>Get your government to submit a case by a petition.</p>
+                    <p>You will need to collect a significant amount of signatures. Then you might be able to strong arm your government intro starting an investigation.</p>
+                  </div>
+                </button>
+              </div>
+
+              <div className={styles.ProcessChoiceContainer}>
+                <button className={individualButtonClass} onClick={() => this.setProcessType('individual')}>
+                  <div className={styles.ProcessChoiceBox}></div>
+                  <div className={styles.ProcessChoiceText}>
+                    <p>Submit evidence as a witness or victim. This is also possible when you know someone who is one of either.</p>
+                    <p>The ICC will send you a form, depending on the crime you reported, which you are required to fill in and send back.</p>
+                  </div>
+                </button>
+              </div>
+            </div>
           </div>
 
           { this.state.process === 'individual' &&
             <ProcessStep>
-              <p>dicks</p>
+              <p>get crackin'</p>
             </ProcessStep>
           }
           { this.state.process === 'country' &&
             <ProcessStep>
-              <p>dicks</p>
+              <p>bitches</p>
             </ProcessStep>
           }
           { !this.state.process &&
             <div className={styles.ProcessNoChoice}>
+              <p>Decide what to do you shall, my padawan.</p>
             </div>
           }
         </main>
