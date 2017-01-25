3961e32397531f6bad2a4c02f8bb74598f9878e1
Nick Rutten <hello@nickrttn.me>
2017-01-25 10:12:24 +0100

    animate the closest case on the worldmap

--- src/components/WorldMap/index.js
+++ src/components/WorldMap/index.js
@@ -7,6 +7,7 @@ import { geoPath, geoMercator } from 'd3-geo';
 import styles from './styles.scss';
 
 import aleppo from './aleppo.jpg';
+import location from './location.svg';
 
 class WorldMap extends Component {
   constructor(props) {
@@ -23,7 +24,6 @@ class WorldMap extends Component {
       'Gabon',
       'Guinea',
       'Iraq',
-      'United Kingdom',
       'Nigeria',
       'Palestine',
       'Ukraine',
@@ -117,15 +117,18 @@ class WorldMap extends Component {
     this.requestUserLocation();
 
     const user = this.User;
-    const warCrimes = Array.from(this.WarCrimes.querySelectorAll('circle'));
+    const warCrimes = Array.from(this.WarCrimes.children);
 
     if (user && warCrimes) {
-      const userX = user.cx.baseVal.value;
-      const userY = user.cy.baseVal.value;
+      const userX = user.transform.baseVal[0].matrix.e;
+      const userY = user.transform.baseVal[0].matrix.f;
+
+      console.dir(user);
 
       warCrimes.forEach((crime) => {
-        const crimeX = crime.cx.baseVal.value;
-        const crimeY = crime.cy.baseVal.value;
+        const crimeX = crime.x.baseVal.value;
+        const crimeY = crime.y.baseVal.value;
+
         crime.distanceToUser = Math.sqrt(
           ((userX - crimeX) * (userX - crimeX)) +
           ((userY - crimeY) * (userY - crimeY))
@@ -136,12 +139,13 @@ class WorldMap extends Component {
         return crimeA.distanceToUser - crimeB.distanceToUser;
       });
 
-      warCrimes[0].style = 'fill: blue;';
+      warCrimes[0].classList.add('closest');
+      warCrimes[0].style.animationPlayState = 'running';
     }
   }
 
   render() {
-    const { geoData } = this.state;
+    const { geoData, userLocation } = this.state;
 
     return (
       <section className={styles.WorldMapContainer}>
@@ -165,20 +169,29 @@ class WorldMap extends Component {
               <image xlinkHref={aleppo} className={styles.WorldMapImage} />
 
               { this.state.userLocation &&
-                <g className={styles.WorldMapUser}>
-                  <circle
-                    ref={(ref) => { this.User = ref; }} r="3"
-                    cx={this.state.userLocation[0]}
-                    cy={this.state.userLocation[1]}
-                  />
+                <g
+                  ref={(ref) => { this.User = ref; }}
+                  transform={`translate(${userLocation[0] - 9} ${userLocation[1] - 35})`}
+                  fill="#0C99E7"
+                  className={styles.WorldMapUser}
+                >
+                  <g fill="#0C99E7" transform="translate(4 13)">
+                    <ellipse cx="6.327" cy="1.802" rx="1.852" ry="1.802"/>
+                    <path d="M10.6206025,1.7078437 C10.9713354,0.546376136 12.7862994,1.06527508 12.4355262,2.22685804 L11.2829341,6.00559012 C11.145827,6.45959299 10.6807443,6.73280591 10.2127675,6.65273371 L8.41741861,6.24591298 L8.43724161,12.1787276 L9.36595528,16.7597297 C9.37684998,16.8140542 9.38258913,16.8681848 9.38354741,16.9216439 L9.64846623,21.0335724 C9.72641762,22.2425012 7.27218603,22.357838 7.19423369,21.1489297 L6.93088549,17.0612495 L6.28008014,13.8511454 L5.62930356,17.0612495 L5.36595536,21.1489297 C5.28800299,22.3578379 2.8337714,22.2425012 2.91172282,21.0335724 L3.1766426,16.9216439 C3.17759992,16.8681847 3.18333906,16.8140542 3.1942242,16.7597297 L4.12294744,12.1787275 L4.14276087,6.24601834 L2.34742156,6.65311788 C1.87944482,6.73319008 1.41436162,6.45997716 1.277255,6.00596495 L0.124859762,2.22705012 C-0.22591292,1.06546741 1.58904192,0.546558687 1.93978491,1.70803578 L2.85058064,4.69467489 C6.20856841,4.11321577 6.35159097,4.1132927 9.70963815,4.69439654 L10.6206025,1.7078437 Z"/>
+                  </g>
                 </g>
               }
 
               <g ref={(ref) => { this.WarCrimes = ref; }} className={styles.WorldMapWarCrimes}>
                 { this.filterGeoData().map(country =>
-                  <g id={country.name} key={country.name} className={styles.WorldMapWarCrime} transform={`translate(${country.center[0] - 11} ${country.center[1] - 30})`}>
-                    <path d="M10.75 0A10.719 10.719 0 0 0 .018 10.732c0 2.07.661 4.047 1.581 5.671l6.994 12.105c.439.79 1.323 1.186 2.157 1.186.834 0 1.674-.396 2.156-1.186L19.9 16.41c.927-1.624 1.581-3.565 1.581-5.671C21.481 4.802 16.687 0 10.75 0zm0 13.902a3.927 3.927 0 0 1-3.918-3.918 3.927 3.927 0 0 1 3.918-3.917 3.927 3.927 0 0 1 3.917 3.917 3.922 3.922 0 0 1-3.917 3.918z" fill="#FFF"/><ellipse stroke="#9D0000" fill="#9D0000" cx="10.814" cy="10.116" rx="3.837" ry="3.837"/>
-                  </g>
+                  <image
+                    xlinkHref={location}
+                    x={country.center[0] - 11}
+                    y={country.center[1] - 30}
+                    className={styles.WorldMapWarCrime}
+                    id={country.name}
+                    key={country.name}
+                  />
                 )}
               </g>
             </svg>
