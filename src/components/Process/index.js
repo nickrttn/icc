d0c2420e6e68a27063291cbc6ad7fe158b826688
Nick Rutten <hello@nickrttn.me>
2017-01-25 16:34:21 +0100

    Preliminary setup for switching process based on user choice

--- src/components/Process/index.js
+++ src/components/Process/index.js
@@ -5,6 +5,13 @@ import ProcessProgress from '../ProcessProgress';
 import ProcessStep from '../ProcessStep';
 
 class Process extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      process: null,
+    }
+  }
 
   render () {
     return (
@@ -14,8 +21,30 @@ class Process extends Component {
           <p className={styles.ProcessHeaderDescription}>This is how <span>you</span> can make the difference</p>
           <p>Follow the interactive process guiding you through all the necessary steps needed to bring a war criminal to justice through the ICC as an individual. You can either explore the process, start a case, or aid one.</p>
         </header>
+
         <ProcessProgress />
-        <main></main>
+
+        <main className={styles.ProcessSteps}>
+          <div className={styles.ProcessFirstStep}>
+            <h3>Choose your path</h3>
+
+          </div>
+
+          { this.state.process === 'individual' &&
+            <ProcessStep>
+              <p>dicks</p>
+            </ProcessStep>
+          }
+          { this.state.process === 'country' &&
+            <ProcessStep>
+              <p>dicks</p>
+            </ProcessStep>
+          }
+          { !this.state.process &&
+            <div className={styles.ProcessNoChoice}>
+            </div>
+          }
+        </main>
       </section>
     );
   }
