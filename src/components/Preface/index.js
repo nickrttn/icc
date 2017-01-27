aaefe9f600a558f7336f30c866d496511150c28d
Merlijn Vos <merlijn.vos043@gmail.com>
2017-01-27 03:26:11 +0100

    Actually add pdf

--- src/components/Preface/index.js
+++ src/components/Preface/index.js
@@ -9,7 +9,7 @@ function Preface() {
       <h2>When a country can not or will not convict its own <span>war criminals</span></h2>
       <p>The ICC is the International Criminal Court. They exist to convict the war criminals that have perpetrated the most egregious crimes.</p>
       <p>The Rome Statute is the leading treaty for the trying of these serious crimes. It includes every crime and the process and evidence regulations. The Rome Statue has been approved by 60 nations since 2002.</p>
-      <a className={styles.button}>Download Rome Statute (PDF)</a>
+      <a className={styles.button} href="./data/Rome_Statute_English.pdf" target="_blank">Download Rome Statute (PDF)</a>
     </section>
   );
 }
